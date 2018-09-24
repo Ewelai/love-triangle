35a2a27ec5857d2bc3d57f9a53ecc1a078d9587e
Ewelai <32734156+Ewelai@users.noreply.github.com>
2018-09-24 16:41:13 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  var preferences;
-  var counter = 0;
-  for (var i = 0; i < preferences.length; i++) {
+  let counter = 0;
+ 
+  for (let i = 0; i < preferences.length; i++) {
       if(preferences[preferences[preferences[i] - 1] - 1] - 1 == i) {
         counter++;
       }
